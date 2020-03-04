6ba905ed466cf4f0e4883499d1fb8a3269d5401e
GloriaBeraldo <gloria.beraldo@phd.unipd.it>
2020-03-04 16:06:35 +0100

    Add the test for SMR BCI

---
+++ src/test_smrbci.cpp
@@ -0,0 +1,33 @@
+#include <ros/ros.h>
+#include "rosneuro_processing/SmrBci.hpp"
+
+int main(int argc, char** argv) {
+
+	
+	// ros initialization
+	ros::init(argc, argv, "smrbci");
+
+	rosneuro::SmrBci smrbci;
+	
+	if(smrbci.configure()== false) {
+		std::cerr<<"SETUP ERROR"<<std::endl;
+		return -1;
+	}
+
+
+	ros::Rate r(50);
+	while(true)
+	{
+
+	 if(smrbci.Classify() == true) {
+	   std::cout <<"Classification" << std::endl; 
+	 } 
+	
+         ros::spinOnce();
+	 r.sleep();
+
+	}
+
+	ros::shutdown();
+	return 0;
+}
